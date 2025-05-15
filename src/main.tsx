738a57f10c0f65bf64681b78ae802dada9c9f575
gouravsasson <sasson.gourav93@gmail.com>
2025-05-15 14:07:07 +0530

    add

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { WidgetProvider } from "./constexts/WidgetContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root")!).render(
+  // <StrictMode>
+  <WidgetProvider
+    agent_id="8bb086ce-0bfa-4fa1-a409-034980c2d914"
+    schema="80ff7fe6-6ea0-478e-b3d6-5690c2d26e0c"
+    type="customwidget"
+  >
     <App />
-  </StrictMode>,
-)
+  </WidgetProvider>
+  // </StrictMode>,
+);
