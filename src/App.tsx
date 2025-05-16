70b118a1ba0f97dc76167198db26de95a08c7a63
gouravsasson <sasson.gourav93@gmail.com>
2025-05-16 16:54:32 +0530

    g

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,20 @@ import axios from "axios";
 import { useEffect } from "react";
 import CustomWidget from "./components/CustomWidget";
 
-useEffect(() => {
-  const localCountryCode = localStorage.getItem("countryCode");
-  if (!localCountryCode) {
-    const fetchIp = async () => {
-      const res = await axios.get("https://ipapi.co/json/");
-      localStorage.setItem("countryCode", res.data.country_calling_code);
-      localStorage.setItem("countryName", res.data.country_name);
-      localStorage.setItem("continentcode", res.data.country);
-      localStorage.setItem("city", res.data.city);
-    };
-    fetchIp();
-  }
-}, []);
-
 function App() {
+  useEffect(() => {
+    const localCountryCode = localStorage.getItem("countryCode");
+    if (!localCountryCode) {
+      const fetchIp = async () => {
+        const res = await axios.get("https://ipapi.co/json/");
+        localStorage.setItem("countryCode", res.data.country_calling_code);
+        localStorage.setItem("countryName", res.data.country_name);
+        localStorage.setItem("continentcode", res.data.country);
+        localStorage.setItem("city", res.data.city);
+      };
+      fetchIp();
+    }
+  }, []);
   return <CustomWidget />;
 }
 
